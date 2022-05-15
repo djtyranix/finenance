1050d45e713c913587ec9fbf663c612652998716
Michael Ricky <37214301+djtyranix@users.noreply.github.com>
2022-05-15 16:44:31 +0700

    Update UI so components are more uniform

---
+++ finenance/Utils/CustomClass/MiniCardView.swift
@@ -0,0 +1,29 @@
+//
+//  MiniCardView.swift
+//  finenance
+//
+//  Created by Michael Ricky on 15/05/22.
+//
+
+import UIKit
+
+@IBDesignable class MiniCardView: UIView {
+
+    var cornerRadius: CGFloat = 12
+    var offsetWidth: CGFloat = 0
+    var offsetHeight: CGFloat = 1
+    var blur: CGFloat = 10
+    
+    var offsetShadowOpacity: Float = 0.8
+    var shadowColor = UIColor.init(hex: "#DDDDDDFF")
+    
+    override func layoutSubviews() {
+        layer.cornerRadius = cornerRadius
+        layer.shadowColor = shadowColor?.cgColor
+        layer.shadowOffset = CGSize(width: offsetWidth, height: offsetHeight)
+        layer.shadowPath = nil
+        layer.shadowRadius = blur / 2.0
+        layer.shadowOpacity = offsetShadowOpacity
+    }
+
+}
